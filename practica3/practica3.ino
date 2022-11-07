214b11f721905edd97015a3c8c0902160f85dede
Fasero11 <86266311+Fasero11@users.noreply.github.com>
2022-11-07 21:16:14 +0100

    Added threads in SERVICIO state

--- practica3/practica3.ino
+++ practica3/practica3.ino
@@ -1,3 +1,4 @@
+
 #include <TimerOne.h>
 #include <LiquidCrystal.h>
 #include <DHT.h>
@@ -7,14 +8,14 @@
 const int ARRANQUE = 0, SERVICIO = 1, ADMIN = 2;
 
 // Variables
-int state = ARRANQUE, ms_timer_start = 0, joystick_read = 0, lcd_cleared = 0, scroll_count = 0, already_scrolled = 0, previous_second = 0;
+int state = ARRANQUE, ms_timer_start = 0, joystick_read = 0, lcd_cleared = 0, scroll_count = 0, already_scrolled = 0, previous_second = 0, make_time = 0;
 
 // Pins
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 10, trigger = 6, echo = 7, DHT_pin = 9, 
-LED_1 = 8, joy_BTN = 2, joy_x = A4, joy_y = A5;
+const int rs = 12, en = 13, d4 = 5, d5 = 4, d6 = 3, d7 = 10, trigger = 6, echo = 7, DHT_pin = 9, 
+LED_1 = 8, joy_BTN = 2, joy_x = A4, joy_y = A5, LED_2 = 11;
 
 // Variables modified in ISR
-volatile byte ledstate = LOW, LED_1_counter = 6, is_client = 0,  item_selected = 0;
+volatile byte ledstate = LOW, LED_1_counter = 6, is_client = 0,  item_selected = 0, BTN_just_pressed = 0;
 
 char *items[] = {"Cafe Solo", "Cafe Cortado", "Cafe Doble", "Cafe Premium", "Chocolate"};
 float prices[] = {1, 1.10, 1.25, 1.50, 2};
@@ -22,7 +23,34 @@ int item_id = 0; // which item is the user on.
 
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 DHT dht(DHT_pin, DHT11);
-Thread myThread = Thread();
+Thread T_H_Thread = Thread();
+Thread Prep_Thread = Thread();
+
+void light_LED2(){
+  analogWrite(LED_2, 255);
+}
+
+void preparando_cafe(){
+  //Serial.println(make_time);
+  Serial.print("Scroll count: ");
+  Serial.println(scroll_count);
+  if (!lcd_cleared){
+    Serial.println("Cleared preparando_café");
+    lcd.clear();
+    lcd_cleared = 1;
+    lcd.setCursor(0,0);
+    lcd.print("Preparando Cafe...");
+    scroll_count = 0;
+  }
+  // There are two extra characters. (Scroll twice)
+  if (scroll_count < 3){
+    if (scroll_text_sec()){
+      scroll_count++;  
+    }     
+  } else {
+    lcd_cleared = 0;
+  }  
+}
 
 int scroll_text_sec(){
   // Scrolls text only once per second.
@@ -41,6 +69,7 @@ int scroll_text_sec(){
 
 void update_joystick_btn(){
   item_selected = 1;
+  BTN_just_pressed = 1;
 }
 
 int update_joystick_y(){
@@ -141,6 +170,7 @@ int detectClient(){
 void setup() {
   Serial.begin(9600);
   pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
   pinMode(trigger, OUTPUT);
   pinMode(echo, INPUT);
   pinMode(joy_BTN , INPUT_PULLUP); 
@@ -150,13 +180,17 @@ void setup() {
   Timer1.initialize(1000000);
   Timer1.attachInterrupt(blinkLED_1);
   
-  myThread.setInterval(100);
-  myThread.onRun(show_T_and_H);
-  myThread.enabled = true;
-
   lcd.begin(16,2);
   dht.begin();
 
+  T_H_Thread.enabled = true;
+  T_H_Thread.setInterval(100);
+  T_H_Thread.onRun(show_T_and_H);
+
+  Prep_Thread.setInterval(10);
+  Prep_Thread.onRun(preparando_cafe);
+  Prep_Thread.enabled = false;
+  
   attachInterrupt(digitalPinToInterrupt(joy_BTN), update_joystick_btn, LOW);
 }
 
@@ -187,28 +221,36 @@ void loop() {
         ms_timer_start = millis();  
       }
     } else {
-        if(millis() - ms_timer_start < 5000){
-          show_T_and_H();
+        if (T_H_Thread.enabled && (millis() - ms_timer_start) / 1000 < 5){
+          if(T_H_Thread.shouldRun()){
+            T_H_Thread.run();
+          }    
         } else {
+          if (T_H_Thread.enabled){
+            T_H_Thread.enabled = false;  
+          }
           if (!item_selected){
-            showItems();           
+            showItems();        
           } else{
-            if (!lcd_cleared){
-              Serial.println("Cleared");
-              lcd.clear();
-              lcd_cleared = 1;
-              lcd.setCursor(0,0);
-              lcd.print("Preparando Cafe...");
-            }
-            // There are two extra characters. (Scroll twice)
-            Serial.println(scroll_count);
-            if (scroll_count < 3){
-              if (scroll_text_sec()){
-                scroll_count++;  
-              }     
-            } else {
+            if (BTN_just_pressed){
+              BTN_just_pressed = 0;
+              make_time = random(4,9);
+              ms_timer_start = millis();
               lcd_cleared = 0;
-              scroll_count = 0;
+              Prep_Thread.enabled = true;
+            }
+            Serial.println("SHOULD RUN");
+            if (Prep_Thread.enabled && (millis() - ms_timer_start)/1000 < make_time){
+              if (Prep_Thread.shouldRun()){
+                Prep_Thread.run(); 
+              }
+            } else {
+              if (Prep_Thread.enabled){
+                Prep_Thread.enabled = false;
+                lcd.clear();
+              }
+              lcd.setCursor(0,0);
+              lcd.print("RETIRE BEBIDA");
             }
           }
         }
