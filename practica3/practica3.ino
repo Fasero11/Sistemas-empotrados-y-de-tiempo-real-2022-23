9ed7a62386fb2deeeb940a8e5b1adb64e7aa7812
Fasero11 <86266311+Fasero11@users.noreply.github.com>
2022-11-07 19:57:23 +0100

    added LCD scrolling

--- practica3/practica3.ino
+++ practica3/practica3.ino
@@ -7,7 +7,7 @@
 const int ARRANQUE = 0, SERVICIO = 1, ADMIN = 2;
 
 // Variables
-int state = ARRANQUE, ms_timer_start = 0, joystick_read = 0, lcd_cleared = 0, scroll_count = 0;
+int state = ARRANQUE, ms_timer_start = 0, joystick_read = 0, lcd_cleared = 0, scroll_count = 0, already_scrolled = 0, previous_second = 0;
 
 // Pins
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 10, trigger = 6, echo = 7, DHT_pin = 9, 
@@ -24,8 +24,19 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 DHT dht(DHT_pin, DHT11);
 Thread myThread = Thread();
 
-void scroll_text(){
-  lcd.scrollDisplayLeft();
+int scroll_text_sec(){
+  // Scrolls text only once per second.
+  // If called twice during the same second, won't do anything.
+  // Returns 1 if text was scrolled, 0 otherwise
+  int was_scrolled = 0;
+  int current_second = (millis() / 1000) % 2;
+
+  if (current_second != previous_second){
+      lcd.scrollDisplayLeft();
+      was_scrolled = 1;  
+  }
+  previous_second = current_second;
+  return(was_scrolled);
 }
 
 void update_joystick_btn(){
@@ -35,11 +46,11 @@ void update_joystick_btn(){
 int update_joystick_y(){
   int updated = 0;
   int val_y = analogRead(joy_y);
-  if (val_y < 100 & !joystick_read){
+  if (val_y > 900 & !joystick_read){
     item_id++;
     updated = 1;
     joystick_read = 1;
-  } else if (val_y > 900 & !joystick_read){
+  } else if (val_y < 100 & !joystick_read){
     item_id--;
     updated = 1;
     joystick_read = 1;
@@ -62,17 +73,39 @@ void blinkLED_1() {
 }
 
 void showItems(){
+  char item_and_price[40]; // 40 is MAX of LCD buffer
+  char str_price[40];
+  int i, str_len, max_len = 0;
+  
   if (update_joystick_y()){
     lcd.clear();
+    scroll_count = 0;
   }
-  Serial.println(item_id);
-  int i;
+  
   for (i = 0; i < 2; i++){
+    dtostrf(prices[item_id+i], 5, 2, str_price); // Cast price from float to string
+    
+    if (i == 0) { // Item selected
+      str_len = snprintf(item_and_price, 25, "--> %s:%s", items[item_id+i], str_price); // Create string containing item and price
+    } else {
+      str_len = snprintf(item_and_price, 25, "    %s:%s", items[item_id+i], str_price); // Create string containing item and price
+    }
     lcd.setCursor(0,i);
-    lcd.print(items[item_id+i]);
+    lcd.print(item_and_price);
+
+    if (str_len > max_len){
+      max_len = str_len;
+    }
   }
-  lcd.setCursor(15,0);
-  lcd.print("*");
+  int scrolls_needed = max_len - 14; // 16 are the LCD positions. We want to leave some space at the end.
+  if (scroll_count < scrolls_needed){
+    if (scroll_text_sec()){
+      scroll_count++;
+    }
+  } else {
+    scroll_count = 0;
+    lcd.clear();
+  }
 }
 
 void show_T_and_H(){
@@ -142,7 +175,6 @@ void loop() {
   }
 
   if (state == SERVICIO){
-    Serial.println(item_selected);
     if (!is_client){
       detectClient();
       lcd.setCursor(0,0);
@@ -157,23 +189,23 @@ void loop() {
     } else {
         if(millis() - ms_timer_start < 5000){
           show_T_and_H();
-          delay(500);
-          lcd.clear();
         } else {
           if (!item_selected){
             showItems();           
           } else{
             if (!lcd_cleared){
+              Serial.println("Cleared");
               lcd.clear();
               lcd_cleared = 1;
               lcd.setCursor(0,0);
               lcd.print("Preparando Cafe...");
             }
             // There are two extra characters. (Scroll twice)
+            Serial.println(scroll_count);
             if (scroll_count < 3){
-              delay(750);
-              scroll_text();
-              scroll_count++;
+              if (scroll_text_sec()){
+                scroll_count++;  
+              }     
             } else {
               lcd_cleared = 0;
               scroll_count = 0;
